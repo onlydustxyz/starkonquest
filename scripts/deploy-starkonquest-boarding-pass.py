045448013c1c6145dfbf3aeb04a23e8de5112e7c
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-02 16:55:14 +0200

    :recycle: Refactor token contracts to the extensibility pattern

--- scripts/deploy-starkonquest-boarding-pass.py
+++ scripts/deploy-starkonquest-boarding-pass.py
@@ -4,13 +4,15 @@ from nile.nre import NileRuntimeEnvironment
 from nile.core.call_or_invoke import call_or_invoke
 
 
-def run(nre : NileRuntimeEnvironment):
+def run(nre: NileRuntimeEnvironment):
 
-    admin = os.environ['ADMIN']
+    admin = os.environ["ADMIN"]
 
     print("Compiling contracts…")
 
-    nre.compile(["contracts/token/StarKonquestBoardingPass.cairo"])
+    nre.compile(
+        ["contracts/token/starkonquest_boarding_pass/starkonquest_boarding_pass.cairo"]
+    )
 
     print("Deploying contracts…")
 
@@ -18,12 +20,17 @@ def run(nre : NileRuntimeEnvironment):
     symbol = str(str_to_felt("SKBP"))
     owner = admin
     params = [name, symbol, owner]
-    address, abi = nre.deploy("StarKonquestBoardingPass", params, alias="starkonquest_boarding_pass")
+    address, abi = nre.deploy(
+        "StarKonquestBoardingPass", params, alias="starkonquest_boarding_pass"
+    )
     print(f"ABI: {abi},\nContract address: {address}")
+
 
 # Auxiliary functions
 def str_to_felt(text):
     b_text = bytes(text, "ascii")
     return int.from_bytes(b_text, "big")
+
+
 def uint(a):
-    return(a, 0)
+    return (a, 0)
