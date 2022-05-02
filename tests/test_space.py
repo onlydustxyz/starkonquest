9dd260d8d7c020b0efb9338e8b14345f7060ed5b
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-02 16:29:54 +0200

    :truck: Rename mock contracts

--- tests/test_space.py
+++ tests/test_space.py
@@ -19,7 +19,7 @@ MAX_FELT = 2**251 + 17 * 2**192 + 1
 
 @pytest.fixture
 async def space_factory(starknet: Starknet) -> StarknetContract:
-    rand = await deploy_contract(starknet, "test/fake_rand.cairo")
+    rand = await deploy_contract(starknet, "mocks/random_mock.cairo")
     space = await deploy_contract(starknet, "core/space/space.cairo")
     await space.initialize(
         rand.contract_address, SPACE_SIZE, MAX_TURN, MAX_DUST
@@ -29,7 +29,7 @@ async def space_factory(starknet: Starknet) -> StarknetContract:
 
 @pytest.mark.asyncio
 async def test_turn_count(starknet: Starknet):
-    rand = await deploy_contract(starknet, "test/fake_rand.cairo")
+    rand = await deploy_contract(starknet, "mocks/random_mock.cairo")
     space = await deploy_contract(starknet, "core/space/space.cairo")
 
     MAX_TURN = 2
